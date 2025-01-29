421601bfeb20fd9506e2d85f7450079d6e39229d
DavidOlmos03 <juanda_201001@hotmail.com>
2025-01-29 13:56:14 -0500

    query scientists_develop created for add scientists

---
+++ scientists_develop.sql
@@ -0,0 +1,10 @@
+INSERT INTO scientists (name, birthday, description, area) 
+VALUES 
+('Albert Einstein', '1879-03-14', 'Developed the theory of relativity', 'Physics'),
+('Charles Darwin', '1809-02-12', 'Proposed the theory of evolution by natural selection', 'Biology'),
+('Nikola Tesla', '1856-07-10', 'Contributions to the design of modern AC electricity supply systems', 'Engineering'),
+('Ada Lovelace', '1815-12-10', 'First computer programmer', 'Computer Science'),
+('Galileo Galilei', '1564-02-15', 'Father of modern observational astronomy', 'Astronomy'),
+('Stephen Hawking', '1942-01-08', 'Theoretical physicist known for black hole radiation', 'Physics'),
+('Rosalind Franklin', '1920-07-25', 'Contributions to the understanding of DNA structure', 'Biology'),
+('Alan Turing', '1912-06-23', 'Father of theoretical computer science and artificial intelligence', 'Computer Science');
